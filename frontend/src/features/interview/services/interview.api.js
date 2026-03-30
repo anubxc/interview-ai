b5f16d09fb0e5449f84f33109fb8d5f969610086
Anubhav <anubhavgupta916@gmail.com>
2026-03-30 19:12:15 +0530

    backend url added

--- frontend/src/features/interview/services/interview.api.js
+++ frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://interview-ai-backend-o16x.onrender.com/',
     withCredentials: true,
 })
 
