833576d9f30db3208d438dfca2a38b4bd8a9a042
Anubhav <anubhavgupta916@gmail.com>
2026-03-30 20:45:23 +0530

    final

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "https://interview-ai-iojs.onrender.com/register",
+  origin: "https://interview-ai-iojs.onrender.com",
   credentials: true
 }));
 
